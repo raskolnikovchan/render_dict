0976d7dd4650cd703771e728e2d15f9e1ae97bd8
raskolnikovchan <raskolnikovkun@gmail.com>
2024-09-28 14:08:57 +0900

    環境変数の修正

--- __init__.py
+++ __init__.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 
 db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_USER")
-db_host = os.getenv("DB_USER")
-db_name = os.getenv("DB_USER")
+db_password = os.getenv("PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
 
 # PostgreSQL接続の設定
 app.config["SQLALCHEMY_DATABASE_URI"] = (
